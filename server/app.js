88436910bf5fafb40c8f9578345920f01daee9ae
abhay zulasana <abhayzulasana@gmail.com>
2025-04-09 19:03:21 +0530

    complate task

--- server/app.js
+++ server/app.js
@@ -1,16 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
 import userRouters from "./router/userRoutes.js";
 
-const app = express();
+dotenv.config(); 
 
+const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+const mongoURI = process.env.MONGO_URI;
 
-mongoose.connect("mongodb://localhost:27017/user-registration", {
+mongoose.connect(mongoURI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 })
@@ -19,4 +22,5 @@ mongoose.connect("mongodb://localhost:27017/user-registration", {
 
 app.use("/api/users", userRouters);
 
-app.listen(5000, () => console.log("Server started on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
